735af6ee4e3b80d0feaf4336601f8a7af219c94d
pprasad <pankajprasad30@gmail.com>
2026-02-26 21:35:25 +0530

    Python data types 26/02/26

--- Basics/Python_Datatype/DataType_Conversion.py
+++ Basics/Python_Datatype/DataType_Conversion.py
@@ -56,3 +56,67 @@ print(b2, type(b2)) # False <class 'bool'>
 a6 = 3
 b3 = bool(a6)
 print(b3, type(b3)) # True <class 'bool'
+
+print("_" * 70)
+############################## Float Data Type ##############################
+# float-> int
+fl2 = 33.33
+in1 = int(fl2)
+print(in1, type(in1)) # 33 <class 'int'>
+
+# float-> string
+fl3 = 33.45
+str1 = str(fl3)
+print(str1, type(str1), str1[-2]) #33.45 <class 'str'> 4
+
+# time : 48:49
+
+# float-> complex
+fl4 = 44.4
+com1 = complex(fl4)
+print(com1, type(com1)) # (44.4+0j) <class 'complex'>
+
+# float-> list --> Conversion is not possible.
+# float-> tuple --> Conversion is not possible.
+# float-> dict --> Conversion is not possible.
+# float-> set --> Conversion is not possible.
+
+# float-> boolean
+fl5 = 44.4
+bool1 = bool(fl4)
+print(bool1, type(bool1)) # True <class 'bool'>
+
+fl6 = 0.000
+bool2 = bool(fl6)
+print(bool2, type(bool2)) # False <class 'bool'>
+
+print("_" * 70)
+############################## String Data Type ##############################
+
+# String-> int
+str1 = 'Hello'
+#num1 = int(str1)
+#print(num1) # ValueError: invalid literal for int() with base 10: 'Hello'
+
+#Note* if string only contains number then conversion is possible.
+str2 = 123
+num2 = int(str2)
+print(num2, type(num2), num2 * 10) # 123 <class 'int'> 1230
+
+# String-> float
+# Note* : if string contains only pointer value then conversion is possible.
+str3 = 44.3
+num3 = float(str3)
+print(num3, type(num3), num3 * 10) #44.3 <class 'float'> 443.0
+
+print("_" * 70)
+# String-> complex
+str5 = '50+60j'
+com5 = complex(str5)
+print(com5, type(com5), com5.real, com5.imag) #(50+60j) <class 'complex'> 50.0 60.0
+
+print("_" * 70)
+# String-> list
+str_a = "Python"
+list_a = list(str_a)
+print(list_a, type(list_a)) # ['P', 'y', 't', 'h', 'o', 'n'] <class 'list'>
---
+++ Basics/Python_Datatype/DataType_Conversion_2.py
@@ -0,0 +1,1 @@
+# Vd 11 : Python Data Type Conversion, Session 2, Revision on 27/02/2026
